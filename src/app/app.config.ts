989546828f8971a0bb2a4365fa7d11cca7965429
nandoProgrammer <nandinhop.costa@gmail.com>
2023-12-21 17:52:51 -0300

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { UrlInterceptor } from './core/interceptors/urlInterceptor';
+
+import { registerLocaleData } from '@angular/common';
+import localePT from '@angular/common/locales/pt';
+registerLocaleData(localePT);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes), 
+    provideHttpClient(withInterceptors([UrlInterceptor])),
+    { provide: LOCALE_ID, useValue: 'pt-br' },
+  ]
 };
